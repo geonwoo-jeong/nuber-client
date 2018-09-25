7f3290087f98e845654618f3e5f4a71b05ecf27f
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-09-25 15:10:46 +0900

    OutHome Component

---
+++ src/Routes/OutHome/OutHomePresenter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+interface IProps extends RouteComponentProps<any> {}
+
+const OutHomePresenter: React.SFC<IProps> = () => <span>stuff</span>;
+
+export default OutHomePresenter;
