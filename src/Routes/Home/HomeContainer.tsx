a42ba0e277092a92ae06333db6ca90d159f74858
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-11-04 15:48:17 +0900

    NearbyDrivers Query part Two

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -72,23 +72,38 @@ class HomeContainer extends Component<IProps, IState> {
     const { isMenuOpen, toAddress, price } = this.state;
     return (
       <ProfileQuery query={USER_PROFILE}>
-        {({ data, loading }) => (
-          <NearbyQueries query={GET_NEARBY_DRIVERS}>
-            {() => (
-              <HomePresenter
-                loading={loading}
-                isMenuOpen={isMenuOpen}
-                toggleMenu={this.toggleMenu}
-                mapRef={this.mapRef}
-                toAddress={toAddress}
-                price={price}
-                data={data}
-                onInputChange={this.onInputChange}
-                onAddressSubmit={this.onAddressSubmit}
-              />
-            )}
-          </NearbyQueries>
-        )}
+        {({ data, loading }) => {
+          if (data && data.GetMyProfile) {
+            const { GetMyProfile: { user = null } = {} } = ({} = data);
+            if (user) {
+              return (
+                <NearbyQueries
+                  query={GET_NEARBY_DRIVERS}
+                  skip={user.isDriving}
+                  onCompleted={this.handleNearbyDrivers}
+                >
+                  {() => (
+                    <HomePresenter
+                      loading={loading}
+                      isMenuOpen={isMenuOpen}
+                      toggleMenu={this.toggleMenu}
+                      mapRef={this.mapRef}
+                      toAddress={toAddress}
+                      price={price}
+                      data={data}
+                      onInputChange={this.onInputChange}
+                      onAddressSubmit={this.onAddressSubmit}
+                    />
+                  )}
+                </NearbyQueries>
+              );
+            } else {
+              return null;
+            }
+          } else {
+            return "Loading";
+          }
+        }}
       </ProfileQuery>
     );
   }
@@ -255,6 +270,16 @@ class HomeContainer extends Component<IProps, IState> {
       this.setState({
         price: Number(parseFloat(distance.replace(",", "")) * 3).toFixed(2)
       });
+    }
+  };
+  public handleNearbyDrivers = (data: {} | getDrivers) => {
+    if ("GetNearbyDrivers" in data) {
+      const {
+        GetNearbyDrivers: { drivers, ok }
+      } = data;
+      if (ok && drivers) {
+        console.log(drivers);
+      }
     }
   };
 }
