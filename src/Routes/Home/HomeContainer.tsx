5951b0601b895294da3c897bfe3a27b6df1fdb79
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-11-08 19:53:48 +0900

    Apollo Subscriptions part One

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -1,3 +1,4 @@
+import { SubscribeToMoreOptions } from "apollo-boost";
 import { geoCode, reverseGeoCode } from "mapHelper";
 import React, { ChangeEvent, Component } from "react";
 import { graphql, Mutation, MutationFn, Query } from "react-apollo";
@@ -22,7 +23,8 @@ import {
   GET_NEARBY_DRIVERS,
   GET_NEARBY_RIDE,
   REPORT_LOCATION,
-  REQUEST_RIDE
+  REQUEST_RIDE,
+  SUBSCRIBE_NEARBY_RIDES
 } from "./HomeQueries.queries";
 
 interface IState {
@@ -129,26 +131,33 @@ class HomeContainer extends Component<IProps, IState> {
               >
                 {requestRideFn => (
                   <GetNearbyRides query={GET_NEARBY_RIDE} skip={!isDriving}>
-                    {({ data: nearbyRide }) => (
-                      <AcceptRide mutation={ACCEPT_RIDE}>
-                        {acceptRideFn => (
-                          <HomePresenter
-                            loading={loading}
-                            isMenuOpen={isMenuOpen}
-                            toggleMenu={this.toggleMenu}
-                            mapRef={this.mapRef}
-                            toAddress={toAddress}
-                            onInputChange={this.onInputChange}
-                            price={price}
-                            data={data}
-                            onAddressSubmit={this.onAddressSubmit}
-                            requestRideFn={requestRideFn}
-                            nearbyRide={nearbyRide}
-                            acceptRideFn={acceptRideFn}
-                          />
-                        )}
-                      </AcceptRide>
-                    )}
+                    {({ subscribeToMore, data: nearbyRide }) => {
+                      const rideSubscriptionOptions: SubscribeToMoreOptions = {
+                        document: SUBSCRIBE_NEARBY_RIDES,
+                        updateQuery: this.handleSubscriptionUpdate
+                      };
+                      subscribeToMore(rideSubscriptionOptions);
+                      return (
+                        <AcceptRide mutation={ACCEPT_RIDE}>
+                          {acceptRideFn => (
+                            <HomePresenter
+                              loading={loading}
+                              isMenuOpen={isMenuOpen}
+                              toggleMenu={this.toggleMenu}
+                              mapRef={this.mapRef}
+                              toAddress={toAddress}
+                              onInputChange={this.onInputChange}
+                              price={price}
+                              data={data}
+                              onAddressSubmit={this.onAddressSubmit}
+                              requestRideFn={requestRideFn}
+                              nearbyRide={nearbyRide}
+                              acceptRideFn={acceptRideFn}
+                            />
+                          )}
+                        </AcceptRide>
+                      );
+                    }}
                   </GetNearbyRides>
                 )}
               </RequestRideMutation>
@@ -398,6 +407,9 @@ class HomeContainer extends Component<IProps, IState> {
         });
       }
     }
+  };
+  public handleSubscriptionUpdate = data => {
+    console.log(data);
   };
 }
 
